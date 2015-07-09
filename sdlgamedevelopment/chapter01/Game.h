a8caa1fa7e6379cee51c45110743557acee97b8c
ivo kovac <mail@ivokovac.info>
2015-07-09 13:25:22 +0200

    Chapter 01 finished.

---
+++ sdlgamedevelopment/chapter01/Game.h
@@ -0,0 +1,30 @@
+#ifndef __Game__
+#define __Game__
+#include <SDL2/SDL.h>
+
+class Game
+{
+
+	public:
+		Game() {}
+		~Game() {}
+
+	// simply set the running variable to true
+	bool init(const char* title, int xpos, int ypos, int width, int height, int flags); 	
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+
+	// a function to access the private running variable
+	bool running() { 
+		return m_bRunning; 
+	}
+
+	private:
+		SDL_Window* m_pWindow;
+		SDL_Renderer* m_pRenderer;
+
+		bool m_bRunning;
+};
+#endif /* defined(__Game__) */
