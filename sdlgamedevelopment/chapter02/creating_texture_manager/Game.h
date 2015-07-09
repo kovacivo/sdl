7ebbb2f3b47984505350d422db74a986e1b9e7d8
ivo kovac <ivo@debivo.kovacivo.edu>
2015-07-09 20:21:12 +0200

    creating texture manager... within 2nd chapter

---
+++ sdlgamedevelopment/chapter02/creating_texture_manager/Game.h
@@ -0,0 +1,34 @@
+#ifndef __Game__
+#define __Game__
+#include <SDL2/SDL.h>
+#include "TextureManager.h"
+
+class Game
+{
+
+	public:
+		Game() {}
+		~Game() {}
+
+	// simply set the running variable to true
+	bool init(const char* title, int xpos, int ypos, int width, int height, int flags); 	
+	void render();
+	void update();
+	void handleEvents();
+	void clean();
+
+	// a function to access the private running variable
+	bool running() { 
+		return m_bRunning; 
+	}
+
+	private:
+		SDL_Window* m_pWindow;
+		SDL_Renderer* m_pRenderer;
+
+		int m_currentFrame;
+		TextureManager m_textureManager;
+
+		bool m_bRunning;
+};
+#endif /* defined(__Game__) */
